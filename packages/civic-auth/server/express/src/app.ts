4c1609d75794555d97a39aa2e3e0d24d285b24c2
lucmir <luccmir@gmail.com>
2024-12-02 10:46:29 -0300

    Initial express sample app

---
+++ packages/civic-auth/server/express/src/app.ts
@@ -0,0 +1,86 @@
+import express, { Request, Response, NextFunction } from 'express';
+import cookieParser from 'cookie-parser';
+import {
+  CookieStorage,
+  resolveOAuthAccessCode,
+  isLoggedIn,
+  getUser,
+  buildLoginUrl,
+  refreshTokens
+} from '@civic/auth/server';
+
+const app = express();
+const PORT = process.env.PORT ?  parseInt(process.env.PORT) : 3000;
+
+// Middleware to parse cookies
+app.use(cookieParser());
+
+const config = {
+  clientId: process.env.CLIENT_ID!,
+  redirectUrl: `http://localhost:${PORT}/auth/callback`,
+  oauthServer: process.env.OAUTH_SERVER ?? 'https://auth.civic.com/oauth',
+};
+
+// Map express cookies to the CookieStorage interface ( using default cookie configuration )
+class ExpressCookieStorage extends CookieStorage {
+  constructor(private req: Request, private res: Response) {
+    super({
+      secure: false
+    })
+  }
+
+  get(key: string): string | null {
+    return this.req.cookies[key];
+  }
+
+  set(key: string, value: string): void {
+    this.res.cookie(key, value, this.settings);
+  }
+}
+
+// Middleware to attach CookieStorage to each request
+app.use((req: Request, res: Response, next: NextFunction) => {
+  req.storage = new ExpressCookieStorage(req, res);
+  next();
+});
+
+// Endpoint to trigger redirect to Civic Auth OAuth server
+app.get('/', async (req: Request, res: Response) => {
+  const url = await buildLoginUrl(config, req.storage);
+
+  res.redirect(url.toString());
+});
+
+// Endpoint to handle OAuth callback and resolve access code
+app.get('/auth/callback', async (req: Request, res: Response) => {
+  const { code, state } = req.query as { code: string; state: string };
+
+  // Resolve OAuth access code and set session
+  await resolveOAuthAccessCode(code, state, req.storage, config);
+  res.redirect('/admin/hello');
+});
+
+// Authentication middleware to protect routes
+const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+  if (!isLoggedIn(req.storage)) return res.status(401).send('Unauthorized');
+  next();
+};
+
+// Apply authentication middleware to /admin routes
+app.use('/admin', authMiddleware);
+
+// Protected route to get logged-in user information
+app.get('/admin/hello', async (req: Request, res: Response) => {
+  const user = await getUser(req.storage);
+  res.send(`Hello, ${user?.name}!`);
+});
+
+app.get('/admin/refresh', async (req: Request, res: Response) => {
+  await refreshTokens(req.storage, config);
+  res.send('Tokens refreshed');
+});
+
+// Start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
